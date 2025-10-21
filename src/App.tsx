0368831f037b8c46f77b4d77dd03fcc55b662027
Kavindu0118 <kavinduavishka0118@gmail.com>
2025-10-21 18:19:49 +0530

    insert pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components'
 import { Home, OurRoles, SoftwareDevelopment } from './screens'
 import HowWeHire from './screens/HowWeHire/HowWeHire'
+import Pricing from './screens/Pricing/Pricing'
 import './App.css'
 
 const App: React.FC = () => {
@@ -14,7 +15,8 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/roles/executive-assistant" element={<OurRoles />} />
           <Route path="/roles/software-development" element={<SoftwareDevelopment />} />
-          <Route path="/HowWeHire" element={<HowWeHire />} />
+          <Route path="/how-we-hire" element={<HowWeHire />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Routes>
       </div>
     </Router>
